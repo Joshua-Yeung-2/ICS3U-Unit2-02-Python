7c31e989beeff584e061038d6251dff4eb4a28df
Ubuntu <ubuntu@ip-172-31-7-96.ec2.internal>
2021-10-21 16:17:25 +0000

    some commit message

---
+++ area_and_perimeter_of_rectangle.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python3
+
+# Created by Joshua Yeung
+# Created on September 2021
+# This program calculates the area and perimeter of a rectangle
+# with radius inputted from the user
+
+
+def main():
+    # this function calculates area and perimeter
+
+    # input
+    length = int(input("Enter length of the rectangle (mm): "))
+    width = int(input("Enter width of the rectangle (mm): "))
+
+    # process
+    area = length * width
+    perimeter = 2 * (length + width)
+
+    # output
+    print("")
+    print("Area is {0} mm².".format(area))
+    print("Perimeter is {0} mm.".format(perimeter))
+
+
+if __name__ == "__main__":
+    main()
